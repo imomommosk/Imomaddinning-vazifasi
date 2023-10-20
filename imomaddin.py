4284d0a201d1c6a17c7169df9711922412f6dc25
imomommosk <148218466+imomommosk@users.noreply.github.com>
2023-10-20 16:51:44 +0500

    mening vazifam

---
+++ imomaddin.py
@@ -0,0 +1,150 @@
+
+
+
+
+ismlar = []
+
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# n=1
+# while True:
+#     savol = f"{n}-do'stingiz ismini kiriting:"
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     javob = input("Yana ism qo'shasizmi? (./yo'q)")
+#     if javob =='.':
+#         n+=1
+#         continue
+#     else:
+#         break
+
+
+
+
+
+# """  UY  ISHI  """
+
+
+# sevimli_taom={
+#     'dadam':'osh',
+#     'onam':'somsa',
+#     'opam':'honim',
+#     'akam':'shashlik',
+#     'men':'lavash'
+# }
+#
+# print(f"\nDadamning savmli taomi {sevimli_taom['dadam']}.\
+#          \nOnamning sevmli taomi {sevimli_taom['onam']}.\
+#          \nAkamning sevli taomi {sevimli_taom['akam']}.")
+
+
+
+
+#
+# malumot={
+#     'ismi':'imomaddin',
+#     'familiasi':'iskandarov',
+#     'kasbi':'maktab oquvchisi',
+#     '1-patqursi':'Beckend dasturlash',
+#     '2-patqursi':"IELTS",
+#     'tug`ilgan yili':'2008',
+#     'tug`ilgan oyi':"8-sentabr",
+#     'yashash xonodoni':'xonqa ko`chasi 63-uy',
+#     'dadasining ismi':"Umid 'shrbekjon' ",
+#     'onasining ismi':'go`zal',
+#     'ukasining ismi':'Samandar'
+# }
+#
+# # print(python_i_lugat)
+# malumot=input("imomaddin haqida malumot: ")
+# if malumot.lower() == 'ismi':
+#     berk='imomaddin'
+# elif malumot.lower() == 'familiasi':
+#     berk='iskandarov'
+#
+#     elif malumot.lower() == 'kasbi':
+#         berk='maktab oquvchisi'
+#
+#     elif malumot.lower() == '1-patqursi':
+#         berk='Backent dasturlash'
+#
+#     elif malumot.lower() == '2-patqursi':
+#         berk="IELTS"
+#
+#     elif malumot.lower() == 'tug`ilgan yili':
+#         berk='2008'
+#
+#     elif malumot.lower() == 'tug`ilgan oyi':
+#      berk="8- sentabir"
+#
+#     elif malumo.lower() == 'yashash xonadoni':
+#         berk='xonqa ko`chasi 63-uy'
+#
+#     elif malumot.lower() == 'dadasinig ismi':
+#         berk="Umid 'sherbekjon' "
+#
+#     elif malumot.lower() == 'onasining ismi':
+#         berk='Gozal'
+#
+#     elif malumot.lower() == 'ukasining ismi':
+#         berk='samandar'
+#
+# else:
+#     print("Bunday atama mavjud emas.")
+# print(berk)
+ismlar = []
+#
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# n=1 # ismlarni sanash uchun o'zgaruvchi
+# while True:
+#     savol = f"{n}-do'stingiz ismini kiriting:"
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     javob = input("Yana ism qo'shasizmi? (ha/yo'q)")
+#     if javob =='ha':
+#         n+=1
+#         continue
+#     else:
+#         break
+#
+# def salom_ber(ism):
+#     """Foydalanuvchi ismini qabul qilib unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+#
+# salom_ber('imomaddin')
+# salom_ber('joxongir')
+# salom_ber('abdulatif')
+# salom_ber('anvar')
+#
+# print(salom_ber.__doc__)
+# def toliq_ism(ism,familia):
+#     """Foydalanuvchini ism familiasini jamlab jqaruvchi funksia"""
+#     print(f"Foydalanuvchning ismi {ism.title()}\n"
+#         f"Foydalanuvchning familiasi {familia.title()}")
+# toliq_ism('olim','hakimov')
+#
+# def yosh_hisobla(tugilgan_yil,joriy_yil=2022):
+#     """Foydalanuvchining tugilgan yilidan hisoblaydi"""
+#     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+# yosh_hisobla(2008,2023)
+
+
+# Ism_fam degan funksiya yarating. Funksiya parametrlari ism, familiya bo'lsin. Familiyaga deaulft qiymat berib keting.
+# Funksiyaga bir agrumentli  hamda ikki argumentli qilib yuklab javobni chiqaring ikkala holatda
+
+# def ism_fam(ism,familiya='iskandarov'):
+#     """Foydalanuvchining ism familiyasin hisoblaydi"""
+#     print(f"{ism.title()}"
+#           f"{familiya.title()}")
+#
+# ism_fam("imomanddin")
+# ism_fam("ghfghfh","fhyfhfhfy")
+#
+# print('salom')
+
+
+
+
+
+
+
+
