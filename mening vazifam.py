3bc7e270971d4e10ee46938545a16cdeb38a869e
imomommosk <148218466+imomommosk@users.noreply.github.com>
2023-10-30 15:45:44 +0500

    Update mening vazifam.py

--- mening vazifam.py
+++ mening vazifam.py
@@ -477,7 +477,7 @@ import random as r
 # ________________________________________________________________________________________________________________________
 import random as r
 
-ismlar = ['olim','anvar','hasan','husan','ali','vali','eshmat','toshmat','rayim',"kimdir"]
+ismlar = ['olim','anvar','hasan','husan','ali','vali','eshmat','toshmat','rayim',"toxir"]
 ism = r.choices(ismlar)
 print(ism)
 print(f"{r.choice(ism)}")
